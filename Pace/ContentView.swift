c6caacf8f9feed08b8bb967157a6fa4b74ac44e1
Dan Corin <dcorin6@gmail.com>
2026-07-16 20:51:51 -0400

    Initial Commit

---
+++ Pace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pace
+//
+//  Created by Daniel Corin on 7/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pace/PaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceApp.swift
+//  Pace
+//
+//  Created by Daniel Corin on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct PaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
